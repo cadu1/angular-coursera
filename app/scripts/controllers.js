7019247b64aa54640e3bd9a7ac0fe4ee4481a4d8
Carlos Oliveira <carlos.eduardo02@gmail.com>
2017-04-18 23:41:45 -0300

    Angular Templates

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -54,7 +54,7 @@ angular.module('confusionApp')
 			console.log($scope.feedback);
 
 			if ($scope.feedback.agree &&
-				($scope.feedback.mychannel == "")) {
+				($scope.feedback.mychannel === "")) {
 				$scope.invalidChannelSelection = true;
 				console.log('incorrect');
 			} else {
@@ -95,5 +95,5 @@ angular.module('confusionApp')
 
 			//Step 5: reset your JavaScript object that holds your comment
 			$scope.comment = { author: "", rating: "5", comment: "" };
-		}
+		};
 	}]);
